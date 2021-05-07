311c638c7b0eca5d0ca43b63e2530dc07e4fa048
Matthew Poetker <matt.pworks@gmail.com>
2021-05-06 19:13:08 -0500

    complete lead implementation

--- lib/pipedrive/resources/lead.rb
+++ lib/pipedrive/resources/lead.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
 module Pipedrive
-  class Lead < Resource; end
+
+  class Lead < Deal
+    include Fields
+
+    def self.fields_url
+      "dealFields"
+    end
+
+    def create(params)
+
+      response = request(
+        :post,
+        resource_url,
+        search_for_fields(params)
+      )
+      new(response.dig(:data))
+    end
+
+    def self.search(term, params = {})
+      puts "Starting Search..."
+      response = request(
+        :get,
+        "#{resource_url}",
+        params
+      )
+      data = response.dig(:data).reject {|lead| lead.dig(:title) != term }
+      data.map {|d| new(d)}
+    end
+
+  end
+
 end
--- lib/pipedrive/util.rb
+++ lib/pipedrive/util.rb
@@ -14,6 +14,7 @@ module Pipedrive
       when 404
         raise NotFoundError.new(response.dig(:error), status)
       else
+        puts response.dig(:error_info)
         raise UnkownAPIError.new(response.dig(:error), status)
       end
     end
